73a65b26455b08d4c21e0d78fd910b4fba10622c
Douglas Snodgrass <103548864+Purplepenguin67@users.noreply.github.com>
2022-10-02 15:10:32 -0700

    cant get server running

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const express = require('express');
+
+require('dotenv').config();
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3004;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use(require("./routes"));
+app.use(require('./routes'));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-network",
-  {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+    useUnifiedTopology: true
+});
 
 
-mongoose.set("debug", true);
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
